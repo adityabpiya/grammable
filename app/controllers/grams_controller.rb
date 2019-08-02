d2947108e2dd10a26c2faf8d97989fc02c32e5d4
adityabpiya <aditya.b.piya@gmail.com>
2019-08-02 03:43:33 +0000

    Validation failed: Email has already been taken

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
 	def destroy
@@ -39,6 +39,9 @@ class GramsController < ApplicationController
 	def edit
 		@gram = Gram.find_by_id(params[:id])
 		return render_not_found if @gram.blank?
+		if @gram.user != current_user
+			render plain: 'Forbidden :(', status: :forbidden
+		end
 	end
 
 	def create
